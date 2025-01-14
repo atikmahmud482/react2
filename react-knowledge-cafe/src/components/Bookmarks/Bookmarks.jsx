9e924ff5046fac669518d138801a2e36fb08eefe
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-14 23:05:25 +0600

    module-42-3-blog data showing

---
+++ react-knowledge-cafe/src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Bookmarks = () => {
+  return (
+    <div className="md:1/3">
+      <h1 className="text-4xl">Bookmarks</h1>
+    </div>
+  );
+};
+
+export default Bookmarks;
