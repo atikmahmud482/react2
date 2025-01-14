7196795082855159af144e6045ae745cd716c647
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-14 22:14:52 +0600

    module-42-1 telwind setup

--- memorable-water-bottale/src/components/Bottle/Bottle.jsx
+++ memorable-water-bottale/src/components/Bottle/Bottle.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import "./Bottle.css";
 const Bottle = ({ bottle, handleAddToCart }) => {
   const { name, img, price } = bottle;
@@ -13,4 +14,8 @@ const Bottle = ({ bottle, handleAddToCart }) => {
   );
 };
 
+Bottle.propTypes = {
+  bottle: PropTypes.array.isRequired,
+  handleAddToCart: PropTypes.func.isRequired,
+};
 export default Bottle;
