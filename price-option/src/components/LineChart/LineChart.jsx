7e3c5ab5a081cf415a3528b2ff3e093322ea9fc4
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-21 00:04:51 +0600

    chart data added

---
+++ price-option/src/components/LineChart/LineChart.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const LineChart = () => {
+  const LineMarksData = [
+    { id: 1, name: "Alice", mathMarks: 85 },
+    { id: 2, name: "Bob", mathMarks: 78 },
+    { id: 3, name: "Charlie", mathMarks: 92 },
+    { id: 4, name: "David", mathMarks: 88 },
+    { id: 5, name: "Eva", mathMarks: 74 },
+    { id: 6, name: "Frank", mathMarks: 95 },
+    { id: 7, name: "Grace", mathMarks: 81 },
+    { id: 8, name: "Hannah", mathMarks: 89 },
+    { id: 9, name: "Ivan", mathMarks: 76 },
+    { id: 10, name: "Jack", mathMarks: 90 },
+  ];
+
+  return <div></div>;
+};
+
+export default LineChart;
