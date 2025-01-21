30686390347a71ba3fb5f7e8d56663f2864096ea
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-21 22:48:07 +0600

    module-45-2

---
+++ introduction-to-react-router/src/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h2>Navbar</h2>
+      <nav>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+        <a href="/contact">contact us</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
--- introduction-to-react-router/src/main.jsx
+++ introduction-to-react-router/src/main.jsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./About/About.jsx";
+import Contact from "./Contact/Contact.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello from react router!!</div>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
   },
   {
     path: "/app",
