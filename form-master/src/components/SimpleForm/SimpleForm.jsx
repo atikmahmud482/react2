72d99ee1c8303d746e84d216e1dc8a8ceb8cc327
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-26 22:39:00 +0600

    form added

---
+++ form-master/src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SimpleForm = () => {
+  return (
+    <div>
+      <h2>Simple Form</h2>
+      <form>
+        <input type="text" />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
