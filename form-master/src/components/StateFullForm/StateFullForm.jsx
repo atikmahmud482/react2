fea37076087e308cb81222a2a7588f33b8b5405f
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-26 23:23:26 +0600

    module 46-2

--- form-master/src/components/StateFullForm/StateFullForm.jsx
+++ form-master/src/components/StateFullForm/StateFullForm.jsx
@@ -1,24 +1,41 @@
 import { useState } from "react";
 
 const StateFullForm = () => {
+  const [name, setName] = useState(null);
   const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(email, password, name);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
 
   const handleEmailChange = (e) => {
     console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
   };
   return (
     <div>
       <h2>Simple Form</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" />
+        <input onChange={handleNameChange} type="text" name="name" />
         <br />
         <input onChange={handleEmailChange} type="email" name="email" />
         <br />
-        <input type="password" name="password" />
+        <input
+          onChange={handlePasswordChange}
+          type="password"
+          name="password"
+        />
         {/* <input type="text" /> */}
         <br />
         <input type="submit" value="Submit" />
