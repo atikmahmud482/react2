d52257b8f1a2111f0c995630fc7eac5ee4d60f71
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-27 23:08:58 +0600

    form style add

--- form-master/src/App.jsx
+++ form-master/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import HookForm from "./components/HookForm/HookForm";
+// import HookForm from "./components/HookForm/HookForm";
+import ReusableForm from "./components/ReusalbleForm/ReusableForm";
 // import RefFrom from "./components/RefForm/RefFrom";
 // import SimpleForm from "./components/SimpleForm/SimpleForm";
 // import StateFullForm from "./components/StateFullForm/StateFullForm";
@@ -11,7 +12,11 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFullForm></StateFullForm> */}
       {/* <RefFrom></RefFrom> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm formTitle={"Sign Up"}></ReusableForm>
+      <ReusableForm
+        formTitle={"Profile Update"}
+        submitBtnText="Update"></ReusableForm>
     </>
   );
 }
--- form-master/src/components/ReusalbleForm/ReusableForm.jsx
+++ form-master/src/components/ReusalbleForm/ReusableForm.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-
-const ReusableForm = () => {
+const ReusableForm = ({ formTitle, submitBtnText = "Submit" }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <form>
+      <h2 className="">{formTitle}</h2>
+      <form
+        onSubmit={handleSubmit}
+        className="space-y-4 p-4 max-w-md mx-auto border rounded-lg shadow">
         <input type="text" name="name" />
         <br />
         <input type="email" name="email" id="" />
         <br />
         <input type="password" name="password" />
         <br />
-        <input type="submit" value="submit" />
+        <input type="submit" value={submitBtnText} />
         <br />
       </form>
     </div>
