4d57756d2b6ca3f2934450cf9ea30ae4523262bc
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-31 18:00:25 +0600

    children adding

--- boi-poka-vibe/src/main.jsx
+++ boi-poka-vibe/src/main.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import Routes and Route
 import Root from "./components/Root/Root";
+import Home from "./components/Home/Home"; // Another component for the Home page
+import About from "./components/About/About"; // About page
+
 import "./index.css";
+import Errorpage from "./components/Errorpage/Errorpage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Root />
+      <Routes>
+        {/* Define routes here */}
+        <Route
+          path="/"
+          element={<Root />}
+          errorElement={<Errorpage></Errorpage>}
+        />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
