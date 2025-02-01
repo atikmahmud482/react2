aa6a9b398330f5a75781417a1462af3a5eae8e9d
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-01 22:48:37 +0600

    card up button added

--- boi-poka-vibe/src/components/Book/Book.jsx
+++ boi-poka-vibe/src/components/Book/Book.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 const Book = ({ book }) => {
-  const { image, bookName, author } = book;
+  const { image, bookName, author, tags } = book;
   return (
     <div className="card bg-base-100 w-96 shadow-xl p-6">
       <figure className="bg-gray-400 py-8 rounded-2xl">
         <img src={image} className="h-[166px]" alt="Shoes" />
       </figure>
       <div className="card-body">
+        <div className="flex justify-center gap-4 ">
+          {tags.map((tag) => (
+            <button className="btn btn-outline">{tag}</button>
+          ))}
+        </div>
         <h2 className="card-title">
           {bookName}
           <div className="badge badge-secondary">NEW</div>
