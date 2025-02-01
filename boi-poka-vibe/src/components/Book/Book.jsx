b72d7069f992414c766f3eb54de7348ec62d3f73
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-01 22:28:36 +0600

    card install to daisi ui

--- boi-poka-vibe/src/components/Book/Book.jsx
+++ boi-poka-vibe/src/components/Book/Book.jsx
@@ -3,8 +3,21 @@ import React from "react";
 const Book = ({ book }) => {
   const { image, bookName, author } = book;
   return (
-    <div>
-      <h2>this is book</h2>
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {bookName}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{author}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
     </div>
   );
 };
