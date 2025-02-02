e5dc679cac46ecf1f1d6d0936b8ea7fad686cd52
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-02 17:33:57 +0600

    module 47-6

--- boi-poka-vibe/src/components/BookDetails/BookDetails.jsx
+++ boi-poka-vibe/src/components/BookDetails/BookDetails.jsx
@@ -7,11 +7,29 @@ const BookDetails = () => {
 
   // console.log(data);
   const book = data.find((book) => book.bookId === id);
-  console.log(book);
-
+  const {
+    bookId: currentBookId,
+    review,
+    category,
+    bookName,
+    author,
+    image,
+  } = book;
   return (
-    <div>
-      <h2>Book Details: {bookId}</h2>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={image} className="max-w-xs rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">{bookName}</h1>
+          <p>By: {author}</p>
+          <div className="border-t-2 border-dashed"></div>
+          <h1 className="text-xl font-bold">{category}</h1>
+          <div className="border-t-2 border-dashed"></div>
+          <p className="py-6">{review}</p>
+          <button className="mr-4 btn btn-soft btn-primary">Read</button>
+          <button className="btn btn-primary">WishList</button>
+        </div>
+      </div>
     </div>
   );
 };
