fa95099fac86414b22595e6b2effd7c79fdf77d9
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-10 23:17:30 +0600

    name , email, photo showing

--- Authentication/src/components/Login.jsx
+++ Authentication/src/components/Login.jsx
@@ -1,21 +1,33 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
+
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         console.log(result);
+        setUser(result.user);
       })
       .catch((error) => {
         console.log("Error", error);
+        setUser(null);
       });
   };
   return (
     <div>
       <button onClick={handleGoogleSignIn}>Login with Google</button>
+      {user && (
+        <div>
+          <h4>{user.displayName}</h4>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
