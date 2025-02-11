583f3f0c037a1f73e358c0d28d3862fe19aa2a94
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-11 22:45:21 +0600

    page creat

--- email-password-auth/src/main.jsx
+++ email-password-auth/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Corrected import
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./Home.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+import Login from "./Login.jsx";
+import Register from "./Register.jsx";
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
