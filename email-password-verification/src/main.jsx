edd6afcb94dc99c55bf6a4d22dc0f135c92ed713
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-12 23:26:17 +0600

    header added

--- email-password-verification/src/main.jsx
+++ email-password-verification/src/main.jsx
@@ -4,13 +4,17 @@ import "./index.css";
 
 import Main from "./layout/Main.jsx";
 import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       {/* Use a layout route to wrap the Home component */}
       <Route path="/" element={<Main />}>
-        <Route index element={<Home />} /> {/* Nested route for Home */}
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Route>
     </Routes>
   </BrowserRouter>
