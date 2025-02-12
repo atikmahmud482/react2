1c16a4be3d4f40167513aaeaab444b456dac5ab7
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-12 23:08:17 +0600

    all set up donee

--- email-password-verification/src/main.jsx
+++ email-password-verification/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Main from "./layout/Main.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  </BrowserRouter>
+);
