ce0ed64cc7d47533ca3f3eb1c5700ba2b4754777
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-12 23:14:34 +0600

    routes setup

--- email-password-verification/src/main.jsx
+++ email-password-verification/src/main.jsx
@@ -3,11 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 
 import Main from "./layout/Main.jsx";
+import Home from "./components/Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Main />} />
+      {/* Use a layout route to wrap the Home component */}
+      <Route path="/" element={<Main />}>
+        <Route index element={<Home />} /> {/* Nested route for Home */}
+      </Route>
     </Routes>
   </BrowserRouter>
 );
