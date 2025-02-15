c1731f26341129d77ba2da008dacf86a589d51aa
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-16 02:58:50 +0600

    video 52-5

--- dragon-news-code/src/components/LatestNews.jsx
+++ dragon-news-code/src/components/LatestNews.jsx
@@ -1,42 +1,20 @@
 import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const LatestNews = () => {
   return (
-    <div className="flex gap-2 items-center">
-      <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest News</p>
-      <Marquee>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit corrupti
-        deserunt praesentium facilis placeat dignissimos vitae explicabo vero
-        obcaecati voluptas? Earum maiores officiis iste impedit, reiciendis
-        ratione maxime labore? Dolor. Amet, fugit unde commodi ullam illo id
-        dicta totam veritatis aspernatur odit quasi deleniti adipisci
-        exercitationem quis voluptate consequuntur rem deserunt ratione omnis
-        tempore eum rerum cumque quidem iste. Officia? Aperiam quae, id eius
-        odit tempore optio. Repudiandae fugit sint ipsum soluta in distinctio
-        deserunt sed, error dolore quam eaque voluptatum vitae dolores ducimus
-        expedita. Dolorum minus consequuntur eaque cupiditate! Nisi molestias
-        magnam eligendi rerum doloremque optio saepe sint, molestiae, pariatur
-        suscipit vel culpa architecto aspernatur reprehenderit vitae fugit nemo
-        vero possimus ad officia sapiente laboriosam ipsum distinctio. Nemo,
-        laboriosam. Sequi neque iusto dolore quos magnam. Mollitia veritatis
-        esse nisi itaque fugit officiis nam qui optio natus, minus voluptates,
-        corrupti, rem modi quis illum fuga maiores consequatur. Omnis, nemo
-        rerum. Ducimus ea nemo neque consequatur cupiditate quas. Natus ea
-        praesentium, facere placeat perspiciatis facilis quod aliquid illo
-        deserunt. Nam, laboriosam impedit officia earum veniam placeat nemo ad
-        eum provident cupiditate. A ipsum doloremque voluptas natus, non
-        quisquam nihil rem ex praesentium? Fugiat non odit sequi iure a sunt
-        harum ratione quasi architecto sit possimus est maiores, sint quis ullam
-        dolore. Voluptatibus autem, velit suscipit distinctio inventore, impedit
-        sit deserunt incidunt aliquid labore reiciendis quas vero tenetur
-        placeat laborum iure ex fugiat voluptates? Hic magni rem accusamus
-        laborum maiores illo odio? Quo eaque sequi fugit laborum, odit sed nisi
-        non natus totam velit dolorum. Ducimus officia dolorum autem doloremque
-        quis repellendus, corporis et deleniti odit vitae asperiores ex.
-        Molestiae, fugiat ratione. A, culpa ea vitae possimus minus eligendi
-        esse aliquam recusandae vel perspiciatis suscipit optio deleniti id
-        animi illum aspernatur, autem corporis sit omnis voluptatum dolores cum
-        cumque. Debitis, eveniet mollitia!
+    <div className="flex gap-2 items-center p-2 ">
+      <p className="bg-[#D72050] text-base-100 px-3 py-1 ">Latest</p>
+      <Marquee className="space-x-10">
+        <Link to="/news">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, dolor?
+        </Link>
+        <Link to="/news">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, dolor?
+        </Link>
+        <Link to="/news">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, dolor?
+        </Link>
       </Marquee>
     </div>
   );
