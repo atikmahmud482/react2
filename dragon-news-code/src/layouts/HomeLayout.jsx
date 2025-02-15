fdbd7ea2acefdf92726e49fda1e50edde961b58a
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-16 02:48:11 +0600

    marque added

--- dragon-news-code/src/layouts/HomeLayout.jsx
+++ dragon-news-code/src/layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 const HomeLayout = () => {
   return (
-    <div>
+    <div className="bg-white text-black">
       <header>
         {" "}
         <Header></Header>
       </header>
+      <section className="w-11/12 mx-auto">
+        <LatestNews></LatestNews>
+      </section>
       <nav></nav>
       <main></main>
     </div>
