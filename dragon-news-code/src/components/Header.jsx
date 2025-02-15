ca530f57376d2971f8e1163350115c3603bfed99
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-16 02:34:03 +0600

    moment added

--- dragon-news-code/src/components/Header.jsx
+++ dragon-news-code/src/components/Header.jsx
@@ -1,5 +1,13 @@
+import logo from "../assets/logo.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 py-2 bg-white ">
+      <div className="logo">
+        <img className="w-[300px]" src={logo} alt="" />
+      </div>
+      <h2 className="text-gray-300">Journalism Without Fear or Favour</h2>
+    </div>
+  );
 };
 
 export default Header;
