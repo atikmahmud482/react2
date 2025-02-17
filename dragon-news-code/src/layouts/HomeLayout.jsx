b1155ef56887872644fe3243c67fb8d0770ca9ef
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-17 23:43:35 +0600

    video 52-6

--- dragon-news-code/src/layouts/HomeLayout.jsx
+++ dragon-news-code/src/layouts/HomeLayout.jsx
@@ -16,10 +16,10 @@ const HomeLayout = () => {
         {" "}
         <Navbar></Navbar>{" "}
       </nav>
-      <main className="w-11/12 mx-auto p-5">
-        <aside>Left Navbar</aside>
-        <section>Main Content</section>
-        <aside>Right Navbar</aside>
+      <main className="w-11/12 mx-auto p-5 grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-3">Left Navbar</aside>
+        <section className="col-span-6">Main Content</section>
+        <aside className="col-span-3">Right Navbar</aside>
       </main>
     </div>
   );
