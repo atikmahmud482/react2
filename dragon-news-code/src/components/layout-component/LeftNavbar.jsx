7203bdf4e838559462f31bc326a0fa38cbd8f7bd
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-17 23:53:12 +0600

    video 52-6

--- dragon-news-code/src/components/layout-component/LeftNavbar.jsx
+++ dragon-news-code/src/components/layout-component/LeftNavbar.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react";
+
 const LeftNavbar = () => {
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+      .then((res) => res.json())
+      .then((data) => setCategory(data.data.news_category));
+  }, []);
   return (
     <div>
       <h2 className="font-bold">All Category</h2>
