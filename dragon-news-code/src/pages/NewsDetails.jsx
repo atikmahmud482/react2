d82fbd755f32a9021782612e727ff74cfd6d9eff
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-19 20:52:35 +0600

    video 52-5-6

--- dragon-news-code/src/pages/NewsDetails.jsx
+++ dragon-news-code/src/pages/NewsDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Header from "../components/Header";
 import RightNavbar from "../components/layout-component/RightNavbar";
 
@@ -14,18 +14,19 @@ const NewsDetails = () => {
       <main className="w-11/12 mx-auto grid grid-cols-12 gap-5">
         <section className="col-span-9">
           <h2 className="font-semibold mb-3">Dragon News</h2>
-          <div className="card bg-white text-black font-poppins bg-base-100 w-full shadow-sm">
+          <div className="card bg-white text-black font-poppins  w-full shadow-sm">
             <figure className="">
               <img src={news?.image_url} alt="Shoes" className="rounded-xl" />
             </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Card Title</h2>
-              <p>
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
-              </p>
+            <div className="card-body ">
+              <h2 className="card-title">{news?.title}</h2>
+              <p>{news?.details}</p>
               <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
+                <Link
+                  to={`/category/${news?.category_id}`}
+                  className="btn btn-primary">
+                  Back to category
+                </Link>
               </div>
             </div>
           </div>
